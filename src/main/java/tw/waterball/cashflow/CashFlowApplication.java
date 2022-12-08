d3d7e1bc70ecf402b891357623314c28ea2f6961
klinkie <klinkie@fake.app>
2022-12-08 19:41:48 +0800

    add hello world api

---
+++ src/main/java/tw/waterball/cashflow/CashFlowApplication.java
@@ -0,0 +1,13 @@
+package tw.waterball.cashflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(CashFlowApplication.class, args);
+    }
+}
---
+++ src/main/java/tw/waterball/cashflow/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package tw.waterball.cashflow.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController
+{
+    @GetMapping(path = "hello", produces = {MediaType.TEXT_PLAIN_VALUE})
+    public String hello()
+    {
+        return "world";
+    }
+}
