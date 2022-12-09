ca185586c875c42fa788c4b8fe888ae399295018
snack7886 <snack7886@gmail.com>
2022-12-09 18:17:25 +0800

    加入 controllor 測試個案

--- src/main/java/tw/waterball/cashflow/controller/HelloWorldController.java
+++ src/main/java/tw/waterball/cashflow/controller/HelloWorldController.java
@@ -5,8 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import tw.waterball.cashflow.CashFlowApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 @RestController
+
+@SpringBootTest(classes = CashFlowApplication.class)
+@AutoConfigureMockMvc
 public class HelloWorldController
 {
     @GetMapping(path = "hello", produces = {MediaType.TEXT_PLAIN_VALUE})
---
+++ src/test/java/tw/waterball/cashflow/controller/HelloWorldControllerTest.java
@@ -0,0 +1,7 @@
+package tw.waterball.cashflow.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldControllerTest {
+
+}
