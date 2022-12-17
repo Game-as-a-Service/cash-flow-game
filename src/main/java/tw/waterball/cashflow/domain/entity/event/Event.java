c3e38fc4db943f722fbf52192fec914ec3ca9f6c
anderson.lin <anderson.lin@asuscloud.com>
2022-12-17 21:42:35 +0800

    feat(extra_payment): 事件(Event) interface 增加事件處理邏輯的 execute() method

--- src/main/java/tw/waterball/cashflow/domain/entity/event/Event.java
+++ src/main/java/tw/waterball/cashflow/domain/entity/event/Event.java
@@ -1,5 +1,19 @@
 package tw.waterball.cashflow.domain.entity.event;
 
+import tw.waterball.cashflow.domain.entity.Actor;
+import tw.waterball.cashflow.domain.entity.exception.InsufficientCashException;
+
 public interface Event {
+    /**
+     * @return 事件類型
+     * @see EventType
+     */
     EventType getEventType();
+
+    /**
+     * 此事件發生的事情
+     * @param actor 玩家
+     * @throws InsufficientCashException 儲蓄不足
+     */
+    void execute(Actor actor) throws InsufficientCashException;
 }
