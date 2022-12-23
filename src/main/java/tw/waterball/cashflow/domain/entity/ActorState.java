2c66ba858e783b4cd91746a8384fc710c632051d
anderson.lin <anderson.lin@asuscloud.com>
2022-12-23 21:32:56 +0800

    refactor(失業(unemployment)): 新增 Actor 屬性與狀態
    * 回合數
    * 骰子數
    * 狀態:無狀態、失業、破產

---
+++ src/main/java/tw/waterball/cashflow/domain/entity/ActorState.java
@@ -0,0 +1,16 @@
+package tw.waterball.cashflow.domain.entity;
+
+public enum ActorState {
+    /**
+     * 無指定狀態
+     */
+    NONE,
+    /**
+     * 失業中
+     */
+    UNEMPLOYMENT,
+    /**
+     * 已破展
+     */
+    BANKRUPTCY
+}
