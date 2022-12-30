69ec7b1e23e6e729f4116034ad6d47a25520c69b
anderson.lin <anderson.lin@asuscloud.com>
2022-12-30 20:20:02 +0800

    refactor(財務支援同項目可多筆): 財務項目在命名上予以分類

--- src/main/java/tw/waterball/cashflow/domain/entity/FinancialItemName.java
+++ src/main/java/tw/waterball/cashflow/domain/entity/FinancialItemName.java
@@ -1,9 +1,16 @@
 package tw.waterball.cashflow.domain.entity;
 
 public enum FinancialItemName {
-    TEACHER_K_12_SALARY,
+    SALARY_TEACHER_K_12,
+    /**
+     * 可轉讓定存單
+     * @see <a href="https://rich01.com/what-is-cds/">https://rich01.com/what-is-cds/</a>
+     */
     CD,
-    CONDO_2_BEDS_1_BATH,
+    /**
+     * 房地產 - 2/1 CONDO
+     */
+    REAL_ESTATE_CONDO_2_BEDS_1_BATH,
     /**
      * 稅
      */
@@ -19,12 +26,12 @@ public enum FinancialItemName {
     BOAT,
     LOAN,
     /**
-     * 股票代號 ON2U
+     * 股票 - ON2U
      */
-    ON2U,
+    STOCK_ON2U,
     /**
-     * 股票代號 OK4U
+     * 股票 - OK4U
      */
-    OK4U,
+    STOCK_OK4U,
     OTHER
 }
