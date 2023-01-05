fb743e646457bc0403993d5b3cba5f47460ea90d
SnackJump <snack7886@gmail.com>
2023-01-06 00:05:53 +0800

    Fix：SettlementDateEvent.java (FinancialStatement改為FinancialStatement2)

--- src/test/java/tw/waterball/cashflow/domain/entity/event/SettlementDateEventTest.java
+++ src/test/java/tw/waterball/cashflow/domain/entity/event/SettlementDateEventTest.java
@@ -3,10 +3,10 @@ package tw.waterball.cashflow.domain.entity.event;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tw.waterball.cashflow.domain.entity.Actor;
-import tw.waterball.cashflow.domain.entity.FinancialStatement;
+import tw.waterball.cashflow.domain.entity.FinancialItem;
+import tw.waterball.cashflow.domain.entity.FinancialItemName;
+import tw.waterball.cashflow.domain.entity.FinancialStatementV2;
 import tw.waterball.cashflow.domain.entity.exception.InsufficientCashException;
-import tw.waterball.cashflow.domain.entity.expense.Expense;
-import tw.waterball.cashflow.domain.entity.expense.ExpenseType;
 
 import java.math.BigDecimal;
 
@@ -20,27 +20,27 @@ class SettlementDateEventTest {
     void giveIncomeMoreThenExpense_whenDrawSettlementDateEvent_thenIncreaseCash() throws InsufficientCashException {
         // Given 玩家A
         Actor actor = new Actor("玩家A", ENGINEER);
-        FinancialStatement financialStatement = actor.getFinancialStatement();
-        BigDecimal totalIncomeAmount = financialStatement.getTotalIncomeAmount();
-        BigDecimal totalExpenseAmount = financialStatement.getTotalExpenseAmount();
+        FinancialStatementV2 financialStatement = actor.getFinancialStatementV2();
+        BigDecimal totalIncomeAmount = financialStatement.getIncome().getTotalIncomeAmount();
+        BigDecimal totalExpenseAmount = financialStatement.getExpense().getTotalExpenseAmount();
         BigDecimal cash = financialStatement.getCash();
 
         // When 玩家A擲骰子，並走到銀行結算日格子
         settlementDateEvent.execute(actor);
 
         // Then 領取
-         BigDecimal finalCash = cash.add(totalIncomeAmount).subtract(totalExpenseAmount);
-        Assertions.assertEquals(finalCash, actor.getFinancialStatement().getCash());
+        BigDecimal finalCash = cash.add(totalIncomeAmount).subtract(totalExpenseAmount);
+        Assertions.assertEquals(finalCash, actor.getFinancialStatementV2().getCash());
     }
 
     @Test
     void giveIncomeLessThenExpense_whenDrawSettlementDateEvent_thenDecreaseCash() throws InsufficientCashException {
         // Given 玩家A
         Actor actor = new Actor("玩家A", ENGINEER);
-        FinancialStatement financialStatement = actor.getFinancialStatement();
-        financialStatement.addExpense(Expense.builder(ExpenseType.INTEREST).amount(BigDecimal.valueOf(920)).build());
-        BigDecimal totalIncomeAmount = financialStatement.getTotalIncomeAmount();
-        BigDecimal totalExpenseAmount = financialStatement.getTotalExpenseAmount();
+        FinancialStatementV2 financialStatement = actor.getFinancialStatementV2();
+        financialStatement.getExpense().addExpense(FinancialItem.builder(String.valueOf(System.currentTimeMillis()), FinancialItemName.CREDIT_CARD, BigDecimal.valueOf(920)).build());
+        BigDecimal totalIncomeAmount = financialStatement.getIncome().getTotalIncomeAmount();
+        BigDecimal totalExpenseAmount = financialStatement.getExpense().getTotalExpenseAmount();
         BigDecimal cash = financialStatement.getCash();
 
         // When 玩家A擲骰子，並走到銀行結算日格子
@@ -48,7 +48,7 @@ class SettlementDateEventTest {
 
         // Then 支付
         BigDecimal finalCash = cash.add(totalIncomeAmount).subtract(totalExpenseAmount);
-        Assertions.assertEquals(finalCash, actor.getFinancialStatement().getCash());
+        Assertions.assertEquals(finalCash, actor.getFinancialStatementV2().getCash());
     }
 
 
@@ -56,10 +56,10 @@ class SettlementDateEventTest {
     void giveExpenseMoreThenIncomePlusCash_whenDrawSettlementDateEvent_thenBankruptcy() {
         // Given 玩家A
         Actor actor = new Actor("玩家A", ENGINEER);
-        FinancialStatement financialStatement = actor.getFinancialStatement();
-        financialStatement.addExpense(Expense.builder(ExpenseType.INTEREST).amount(
-                financialStatement.getCash().add(financialStatement.getTotalIncomeAmount())
-        ).build());
+        FinancialStatementV2 financialStatement = actor.getFinancialStatementV2();
+        financialStatement.getExpense().addExpense(
+                FinancialItem.builder(String.valueOf(System.currentTimeMillis()), FinancialItemName.CREDIT_CARD, financialStatement.getCash().add(
+                        financialStatement.getIncome().getTotalIncomeAmount())).build());
 
         // When 玩家A擲骰子，並走到銀行結算日格子 Then 破產
         Assertions.assertThrows(InsufficientCashException.class, () -> settlementDateEvent.execute(actor));
